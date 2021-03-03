3f914581de8dd5d7e1445672be9c9fb12543379c
seryl lounis <seryllounis@outlook.fr>
2021-03-03 14:18:00 +0100

    Finish with good style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.delete_all
+
+
+Restaurant.create!(
+  name: "la Farandole",
+  address: "12 rue de la farandole",
+  phone_number: "0412121212",
+  category: "french"
+)
+Restaurant.create!(
+  name: "Ping pong",
+  address: "8 av de la chine",
+  phone_number: "0456764234",
+  category: "chinese"
+)
+Restaurant.create!(
+  name: "Chez luigi",
+  address: "4 boulevard de l'italie",
+  phone_number: "0413232323",
+  category: "italian"
+)
+Restaurant.create!(
+  name: "Sushi shop",
+  address: "Chez wam",
+  phone_number: "0413421212",
+  category: "japanese"
+)
+Restaurant.create!(
+  name: "Des Frites Des Frites Des Frites",
+  address: "6 rue de la frite",
+  phone_number: "0434658624",
+  category: "belgian"
+)
